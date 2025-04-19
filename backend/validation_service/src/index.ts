e0d41587ae3a7812e618ef319564eb59966d8299
Manish bhukar <125037273+manish-bhukar@users.noreply.github.com>
2025-04-20 04:30:20 +0530

    validation done in backend

---
+++ backend/validation_service/src/index.ts
@@ -0,0 +1,42 @@
+import express,{Request,Response} from "express";
+import dotenv from "dotenv";
+import { validateConfig } from "./validate-config";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.json({
+        message: "Validation Service is running",})
+})
+
+app.post("/validate", async (req:Request,res:Response):Promise<any>=>{
+    const {config} = req.body;/
+    
+    console.log(config)
+    if(!config){
+        return res.status(400).json({error: "Configuration file is required"});
+    }
+    const serviceType = config.type;
+    if(!serviceType){
+        return res.status(400).json({error: "Service type is required"});
+    }
+
+    try{
+        const response = await validateConfig(config);
+        res.status(200).json(response);
+    }
+    catch(error:any){
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+    
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Validation Service is running on port ${process.env.PORT}`);
+})
+
+
